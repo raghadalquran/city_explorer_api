cb8e39619b5f98b72436eb41f9928b4a588fd88e
raghadalquran <raghadalquran1@gmail.com>
2020-04-05 15:12:31 +0300

    still working

---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict';
+// Load Environment Variables from the .env file
+require('dotenv').config();
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+
+// Application Setup
+const PORT = process.env.PORT || 4000;
+const app = express();
+app.use(cors());
+// API Routes
+app.get('/', (request, response) => {
+  response.status(200).send('It Works!');
+});
+app.get('/bad', (request, response) => {
+  throw new Error('oh nooooo!');
+});
+
+app.get('/location', (request, response) => {
+  try {
+    const geoData = require('./data/geo.json');
+    const city = request.query.city;
+    const locationData = new Location(city, geoData);
+    response.status(200).json(locationData);
+  } catch (error) {
+    errorHandler(error, request, response);
+  }
+});
+app.use('*', notFoundHandler);
+function Location(city, geoData) {
+  this.search_query = city;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
+}
+// HELPER FUNCTIONS
+function notFoundHandler(request, response) {
+  response.status(404).send('NOT FOUND!!');
+}
+function errorHandler(error, request, response) {
+  response.status(500).send(error);
+}
+// Make sure the server is listening for requests
+app.listen(PORT, () => console.log(`the server is up and running on ${PORT}`));
